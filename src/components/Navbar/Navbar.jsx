009bcf3f6169f7592f5fb46f34ac349c4ebd3a79
Santiago <santiagobalino@hotmail.com>
2023-01-13 18:24:37 -0300

    Add Navbar component and chakra ui icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,44 @@
+import { BellIcon, EmailIcon, SettingsIcon } from '@chakra-ui/icons';
+import { Box, Button, Heading, Stack } from '@chakra-ui/react';
 import React from 'react';
 
 export const Navbar = () => {
-    return <div>Navbar</div>;
+    return (
+        <>
+            <Box alignItems="center" bg="#090924" height="5vh" width="100vw">
+                <Stack
+                    alignItems="center"
+                    bg="primary"
+                    color="white"
+                    display="flex"
+                    flexDirection="row"
+                    height="calc(100% - 1px)"
+                    justifyContent="space-between"
+                    zIndex="15"
+                >
+                    <Heading ml="100px">Hola, soy un texto</Heading>
+                    <Stack
+                        alignItems="center"
+                        flexDirection="row"
+                        justifyContent="space-evenly"
+                        minWidth="350px"
+                        spacing="0"
+                    >
+                        <BellIcon height="20px" width="20px" />
+                        <SettingsIcon height="20px" width="20px" />
+                        <EmailIcon height="20px" width="20px" />
+                        <Button
+                            bg="highlight"
+                            color="white"
+                            p="12px 12px"
+                            variant="solid"
+                            width="35%"
+                        >
+                            Click Here
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Box>
+        </>
+    );
 };
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,9 +2,9 @@ import { extendTheme } from '@chakra-ui/react';
 
 export const myNewTheme = extendTheme({
     colors: {
-        primary: '#642CA9',
-        secondary: '#625776',
-        terciary: '#1B1725',
+        primary: '#070720',
+        secondary: '#0D0D28',
+        terciary: '#5C5C8E',
         danger: '#EE1739',
         highlight: '#F62F63',
     },
