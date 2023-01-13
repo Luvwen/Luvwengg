debb880bd68d77f70515cc5dd6592cf823dc532e
Santiago <santiagobalino@hotmail.com>
2023-01-12 23:22:28 -0300

    Setup eslint, chakra ui and prettier

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { ChakraBaseProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <ChakraBaseProvider>
+            <App />
+        </ChakraBaseProvider>
+    </React.StrictMode>
+);
