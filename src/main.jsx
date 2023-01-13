4047cd1c9ae868c162363c1313b981870b22696a
Santiago <santiagobalino@hotmail.com>
2023-01-13 00:13:00 -0300

    Redux and React Redux config

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { ChakraBaseProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ChakraBaseProvider>
-            <App />
-        </ChakraBaseProvider>
+        <Provider store={store}>
+            <ChakraBaseProvider>
+                <App />
+            </ChakraBaseProvider>
+        </Provider>
     </React.StrictMode>
 );
