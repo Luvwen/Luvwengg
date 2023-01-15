317d124c385afcf52945a977977d58558e55a5c4
Santiago <santiagobalino@hotmail.com>
2023-01-14 23:46:20 -0300

    Add react-router-dom and env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Route, Routes } from 'react-router-dom';
+import { Champions } from './components/Champions/Champions';
 import { Home } from './components/Home';
 import { Navbar } from './components/Navbar/Navbar';
 import { Sidebar } from './components/Sidebar/Sidebar';
@@ -6,8 +8,11 @@ function App() {
     return (
         <>
             <Navbar />
+            <Routes>
+                <Route element={<Champions />} path="/champions" />
+                <Route element={<Home />} path="/*" />
+            </Routes>
             <Sidebar />
-            <Home />
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import { ChakraBaseProvider } from '@chakra-ui/react';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { ChakraBaseProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
-import App from './App';
 import { store } from './app/store';
+import App from './App';
+
 import { myNewTheme } from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <ChakraBaseProvider theme={myNewTheme}>
-                <App />
-            </ChakraBaseProvider>
-        </Provider>
+        <BrowserRouter>
+            <Provider store={store}>
+                <ChakraBaseProvider theme={myNewTheme}>
+                    <App />
+                </ChakraBaseProvider>
+            </Provider>
+        </BrowserRouter>
     </React.StrictMode>
 );
